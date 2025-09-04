fe15c13a393ee70ca90bd589c35822a586679708
G-Love53 <G@CommercialInsurance-Direct.com>
2025-09-04 12:40:34 -0600

    Implement email sending function using nodemailer

--- src/email.js
+++ src/email.js
@@ -1,1 +1,17 @@
+import nodemailer from "nodemailer";
+
+export async function sendWithGmail({ to, subject, html, attachments }) {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: { user: process.env.GMAIL_USER, pass: process.env.GMAIL_APP_PASSWORD }
+  });
+
+  await transporter.sendMail({
+    from: process.env.GMAIL_USER,
+    to,
+    subject,
+    html,
+    attachments: attachments.map(a => ({ filename: a.filename, content: a.buffer }))
+  });
+}
 
